0dad458f093868a90b29deef64dfe53b2831cb3a
BlueBearrii <thananan.math.acs@gmail.com>
2020-10-04 23:45:09 +0700

    :gem: Create register route

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const func = require("./register");
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.post("/register", (req, res) => {
+  var name = req.body.name;
+  const user = {
+    name: name,
+  };
+
+  func.register(name)
+    ? res.status(201).json({ message: " Registered " })
+    : res.status(400).json({ message: " Something went wrong " });
 });
 
 const PORT = 5000;
