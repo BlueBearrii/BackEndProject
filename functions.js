d32bb3ee3d1ac56e1a598022a20498987c193d7e
BlueBearrii <thananan.math.acs@gmail.com>
2020-10-08 11:20:20 +0700

    :zap: Function register

---
+++ functions.js
@@ -0,0 +1,19 @@
+var _ = require("lodash");
+
+exports.register = function (data) {
+  const user = data;
+  var errors = {};
+  var regEx = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+  // Check isEmpty
+  if (_.isEmpty(user.username)) errors.username = "username is empty";
+  if (_.isEmpty(user.password)) errors.password = "password is empty";
+  if (_.isEmpty(user.firstName)) errors.firstName = "firstname is empty";
+  if (_.isEmpty(user.lastName)) errors.lastName = "lastname is empty";
+  if (_.isEmpty(user.email)) errors.email = "email is empty";
+
+  // Check regEx
+  if (!user.email.match(regEx)) errors.email = "invalid email";
+
+  return errors;
+};
